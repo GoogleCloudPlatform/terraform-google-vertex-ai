73185d075cb3093a3c1538927fdef49496467d61
Ivan Chornyi <paalomnikdev@users.noreply.github.com>
2024-08-30 11:41:34 +0300

    Fix accelerator_configs var validation

--- modules/workbench/variables.tf
+++ modules/workbench/variables.tf
@@ -130,7 +130,7 @@ variable "accelerator_configs" {
   }))
   default = null
   validation {
-    condition     = try(alltrue([for rp in var.accelerator_configs : contains(["NVIDIA_TESLA_P100", "NVIDIA_TESLA_V100", "NVIDIA_TESLA_P4", "NVIDIA_TESLA_T4", "NVIDIA_TESLA_A100", "NVIDIA_A100_80GB", "NVIDIA_L4", "NVIDIA_TESLA_T4_VWS", "NVIDIA_TESLA_P100_VWS", "NVIDIA_TESLA_P4_VWS"], var.accelerator_configs.type)]), false) || var.accelerator_configs == null
+    condition     = try(alltrue([for rp in var.accelerator_configs : contains( ["NVIDIA_TESLA_P100", "NVIDIA_TESLA_V100", "NVIDIA_TESLA_P4", "NVIDIA_TESLA_T4", "NVIDIA_TESLA_A100", "NVIDIA_A100_80GB", "NVIDIA_L4", "NVIDIA_TESLA_T4_VWS", "NVIDIA_TESLA_P100_VWS", "NVIDIA_TESLA_P4_VWS"], rp.type)]), false) || var.accelerator_configs == null
     error_message = "accelerator_configs.type must be one of [NVIDIA_TESLA_P100, NVIDIA_TESLA_V100, NVIDIA_TESLA_P4, NVIDIA_TESLA_T4, NVIDIA_TESLA_A100, NVIDIA_A100_80GB, NVIDIA_L4, NVIDIA_TESLA_T4_VWS, NVIDIA_TESLA_P100_VWS, NVIDIA_TESLA_P4_VWS]"
   }
 }
