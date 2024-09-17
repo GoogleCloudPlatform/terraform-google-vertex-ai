9d0c65966bed87429135f856976aba91a525168b
randyzwguo@qq.com <randyzwguo@qq.com>
2024-09-17 08:35:25 +0800

    initiation

---
+++ test-project-ai/main.tf
@@ -0,0 +1,50 @@
+/*
+ *
+ * Author :  Randy Guo (randyzwguo@qq.com) 
+ *
+ */
+
+
+
+resource "google_service_account" "workbench_sa" {
+  account_id   = "vertex-ai-workbench-sa"
+  display_name = "Vertex AI Workbench Service Account"
+  project      = var.project_id
+}
+
+module "simple_vertex_ai_workbench" {
+  source  = "GoogleCloudPlatform/vertex-ai/google//modules/workbench"
+  version = "~> 0.1"
+
+  name       = "simple-vertex-ai-workbench"
+  location   = var.workbench_region
+  project_id = var.project_id
+
+  machine_type = "e2-standard-2"
+
+  labels = {
+    env  = "test"
+    type = "workbench"
+  }
+
+  service_accounts = [
+    {
+      email = google_service_account.workbench_sa.email
+    },
+  ]
+
+  data_disks = [
+    {
+      disk_size_gb = 330
+      disk_type    = "PD_BALANCED"
+    },
+  ]
+
+  network_interfaces = [
+    {
+      network  = module.test-vpc-module.network_id
+      subnet   = module.test-vpc-module.subnets_ids[0]
+      nic_type = "GVNIC"
+    }
+  ]
+}
---
+++ test-project-ai/network.tf
@@ -0,0 +1,25 @@
+/*
+ *
+ * Author :  Randy Guo (randyzwguo@qq.com) 
+ *
+ */
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+module "test-vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 9.0"
+  project_id   = var.project_id
+  network_name = "simple-workbench"
+  mtu          = 1460
+  subnets = [
+    {
+      subnet_name           = "simple-subnet-01"
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = var.workbench_region
+      subnet_private_access = true
+    },
+  ]
+}
